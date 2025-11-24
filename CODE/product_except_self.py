28e6d0bfad55695e7f5657514a91606370f378a6
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2025-11-24 15:22:06 +0200

    2 more tests in CODE/product_except_self.py

--- CODE/product_except_self.py
+++ CODE/product_except_self.py
@@ -30,10 +30,12 @@ def product_except_self(arr: list) -> int:
         
 
 def test__product_except_self():
-    arr1 = [1, 2, 3]  # Output: [6, 3, 1]
-    arr2 = [10, 3, 5, 6, 2]  # Output: [180, 600, 360, 300, 900]
-    arr3 = [12, 0]  # Output: [0, 12]
-    for arr in [arr1, arr2, arr3]:
+    arr1 = [1, 2, 3]          # Output: [6, 3, 1]
+    arr2 = [10, 3, 5, 6, 2]   # Output: [180, 600, 360, 300, 900]
+    arr3 = [12, 0]            # Output: [0, 12]
+    arr4 = [1, 2, 3, 4]       # Output: [24,12,8,6]
+    arr5 = [-1, 1, 0, -3, 3]  # Output: [0,0,9,0,0]
+    for arr in [arr1, arr2, arr3, arr4, arr5]:
         print("=========================")
         print(f"Input:  {arr}")
         res = product_except_self(arr)
