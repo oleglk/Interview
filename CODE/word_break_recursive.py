00516895f178457b711455638d8f44339be100c9
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2025-12-04 13:11:59 +0200

    ONGOING: In CODE/word_break_recursive.py pass prefix length and unmodified string

--- CODE/word_break_recursive.py
+++ CODE/word_break_recursive.py
@@ -10,12 +10,13 @@ prefixMemo = [];  # for memoization - -1|0|1 indexed by prefix length
 
 def word_break_recursive(s: str, wordDict: dict) -> bool:
     prefixMemo = [-1]*(len(s)+1)  # length from 0 to len() inclusive
-    return(can_segment_recursive(s, wordDict, prefixMemo))
+    return(can_segment_recursive(s, len(s), wordDict, prefixMemo))
 
 
-def can_segment_recursive(s: str, wordDict: dict, memo: list) -> bool:
-    if ( s == '' ):
+def can_segment_recursive(s: str, sLen: int, wordDict: dict, memo: list) -> bool:
+    if ( (s == '') or (sLen == 0) ):
         return(1)  # base case - string either was empty or depleted
+    s = s[:sLen]   # this iteration takes 'sLen' first characters
     for word in wordDict:
         if ( not s.endswith(word) ):
             continue
@@ -27,7 +28,7 @@ def can_segment_recursive(s: str, wordDict: dict, memo: list) -> bool:
            canDoPrefix = memo[prefLength]
            print(f"@@@@ Used memoized answer={canDoPrefix} for prefix '{prefix}'")
         else:
-            canDoPrefix = can_segment_recursive(prefix, wordDict, memo)
+            canDoPrefix = can_segment_recursive(s, prefLength, wordDict, memo)
             memo[prefLength] = canDoPrefix
         if ( canDoPrefix ):
             return(1)
@@ -35,7 +36,7 @@ def can_segment_recursive(s: str, wordDict: dict, memo: list) -> bool:
 
 
 def test__word_break_recursive():
-    tasks = [ ["thesong", {"the":1, "song":1, "break":1}],
+    tasks = [ ["thesong", {"the":1, "song":1, "break":1, "can":1, "long":1}],
               ["ashape", {"a":1, "share":1}],
               ["catsanddog", {"cat":1, "cats":1, "and":1, "sand":1, "dog":1}],
               ["ilike", {"i":1, "like":1, "gfg":1}],
