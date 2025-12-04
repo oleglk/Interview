97f3e6a2e8331df90062ef638f398076c309eb82
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2025-12-04 12:50:27 +0200

    In CODE/word_break_recursive.py memoize by prefix length

---
+++ CODE/word_break_recursive.py
@@ -0,0 +1,48 @@
+# word_break.py - Given a string s and a dictionary of strings wordDict, determine if s can be segmented into a space-separated sequence of one or more dictionary words. Words in string s ARE NOT DELIMITED ANYHOW"
+
+# LOAD:
+# import sys;  import os;  sys.path.insert(0, os.getcwd());  from word_break_recursive import *
+
+# RELOAD:
+# import importlib; import word_break_recursive; importlib.reload(word_break_recursive); from word_break_recursive import *
+
+prefixMemo = [];  # for memoization - -1|0|1 indexed by prefix length
+
+def word_break_recursive(s: str, wordDict: dict) -> bool:
+    prefixMemo = [-1]*(len(s)+1)  # length from 0 to len() inclusive
+    return(can_segment_recursive(s, wordDict, prefixMemo))
+
+
+def can_segment_recursive(s: str, wordDict: dict, memo: dict) -> bool:
+    if ( s == '' ):
+        return(1)  # base case - string either was empty or depleted
+    for word in wordDict:
+        if ( not s.endswith(word) ):
+            continue
+        # the current substring ends with 'word'; check if prefix is segmentable
+        prefix = s[:-len(word)]  # if 'word' truncated
+        prefLength = len(prefix)
+        print(f"@@ Substring '{s}': prefix='{prefix}', ends with '{word}'")
+        if ( memo[prefLength] != -1 ):
+           canDoPrefix = memo[prefLength]
+           print(f"@@@@ Used memoized answer={canDoPrefix} for prefix '{prefix}'")
+        else:
+            canDoPrefix = can_segment_recursive(prefix, wordDict, memo)
+            memo[prefLength] = canDoPrefix
+        if ( canDoPrefix ):
+            return(1)
+    return(0)
+
+
+def test__word_break_recursive():
+    tasks = [ ["thesong", {"the":1, "song":1, "break":1}],
+              ["ashape", {"a":1, "share":1}],
+              ["catsanddog", {"cat":1, "cats":1, "and":1, "sand":1, "dog":1}],
+              ["ilike", {"i":1, "like":1, "gfg":1}],
+              ["ilikegfg", {"i":1, "like":1, "man":1, "india":1, "gfg":1}],
+              ["ilikemangoes", {"i":1, "like":1, "gfg":1}] ]
+    for s, wordDict  in tasks:
+        print("===========================")
+        print(f"Input: '{s}' with {wordDict}")
+        res = word_break_recursive(s, wordDict)
+        print(f"Result: {res == 1}")
