3333bd4daec222ebdffe5f59f38adb4cb2ae915b
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-01-14 12:39:53 +0200

    Working CODE/reservoir_sampling.py

---
+++ CODE/reservoir_sampling.py
@@ -0,0 +1,36 @@
+# reservoir_sampling.py - Uniformly select one element from the stream of data.
+
+# LOAD:
+# import sys;  import os;  sys.path.insert(0, os.getcwd());  from reservoir_sampling import *
+
+# RELOAD:
+# import importlib; import reservoir_sampling; importlib.reload(reservoir_sampling); from reservoir_sampling import *
+
+# The idea: pick #i-s element with probability of 1/i .
+# Correctness proved by induction.
+# Base case - n = 1, thus p = 1 - OK
+# Assume uniformity for stream size of n, check uniformity for n+1:
+# - element #n+1 selected with probability 1/(n+1)
+# - any previous element selected with probability 1/n * (1 - 1/(n+1))
+## 1/n * (1 - 1/(n+1)) == 1/n * n/(n+1) == 1/(n+1) <<<< uniformity proven
+
+import random
+
+def reservoir_sampling(iterableStream):
+    res = None
+    for (i, x) in enumerate(iterableStream):
+        if ( random.random() < 1/(i+1) ):  # i=0>p=1, i=1>p=1/2, ...
+            res = x
+    return res
+
+
+def test__reservoir_sampling():
+    stream = [0,1,2,3,4,5,6,7,8,9]
+    freq = [0]*len(stream)
+    print("=======================")
+    for _ in range(0, len(stream)*100):
+        res = reservoir_sampling(stream)
+        freq[res] += 1
+    print(f"Result: {freq}")
+
+            
