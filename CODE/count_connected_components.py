d59baed9957ba04cbacd63833c5bed6909b3bc05
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-01-20 11:45:54 +0200

    Working CODE/count_connected_components.py

--- CODE/count_connected_components.py
+++ CODE/count_connected_components.py
@@ -12,12 +12,56 @@
 from collections import defaultdict
 
 class Solution:
-    def __init__(n, edges):
+    def __init__(self, n: int, edges: list[int]):
         self.numNodes = n
         self.parent = list(range(0, n)) # initially each node is parent of itself
-        self.compDict = defaultdict(list)
+        self.edges = edges
+        # build the actual components
+        for u, v  in self.edges:
+            self.merge(u, v)
+            
+        
+    def find_parent(self, x: int) -> int:
+        """Finds parent of element x.
+           Optimizes the array by path compression while searching"""
+        if ( x != self.parent[x] ):
+            self.parent[x] = self.find_parent(self.parent[x])
+        return self.parent[x]
 
-    def find(x: int) -> int:
-        """Finds parent of element x. Optimizes the array while searching"""
-        if ( x != 
-    
+
+    def merge(self, x: int, y: int):
+        """Makes root of y son of root of x"""
+        parX = self.find_parent(x)
+        parY = self.find_parent(y)
+        if ( parX != parY ):
+            self.parent[parY] = parX
+
+
+    def count(self) -> int:
+        compDict = defaultdict(list)
+        for node, par  in enumerate(self.parent):
+            #compDict[par].append(node)  # assuming all paths compressed
+            compDict[self.find_parent(par)].append(node)
+        return len(compDict.keys())
+        
+
+    @staticmethod
+    def count_components(n: int, edges: list[int]) -> int:
+        sln = Solution(n, edges)
+        return sln.count()
+    #########################################
+
+
+def test__count_components():
+    tasks = [
+        [2, [(0,1)]],               # 1
+        [1, []],                    # 1
+        [4, [(0,1), (2,3)]],        # 2
+        [5, [(1,0), (2,3), (3,4)]]  # 2
+    ]
+    for n, edges  in tasks:
+        print("=======================================")
+        print(f"Input: n={n}, edges={edges}")
+        res = Solution.count_components(n, edges)
+        print(f"Result: {res}")
+
