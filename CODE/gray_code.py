d0bb750414a7322c773a2c0c8f7a31396305329a
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-02-12 11:38:52 +0200

    Comments in CODE/gray_code.py

--- CODE/gray_code.py
+++ CODE/gray_code.py
@@ -7,8 +7,15 @@
 # import importlib; import gray_code; importlib.reload(gray_code); from gray_code import *
 
 
+# In Gray code consecutive numbers in the sequence differ by exactly one bit in their binary representation.
+
 # The idea: recursively obtain solution list for one bit less (prev-list), prepend 0 to prev-list elements, prepend 1 to reversed-copy-of prev-list elements, concatenate the two lists.
 
+# n = 1: [0, 1]
+# n = 2: [00, 01, !  11, 10]
+# n = 3: [000, 001, 011, 010, !  110, 111, 101, 100]
+# It's obvious that "inside" the sequence with prepended 0 or 1 only 1 bit changes in consequtive numbers.
+# The boundary btw these sequences is 0[num] and 1[num], thus again only 1 bit changes.
 
 def gray_codes(nBits: int) -> list[int]:
     if ( nBits == 1 ):
