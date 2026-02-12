37f8956d163752177ba176bb6ecb516d52d7620d
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-02-12 11:00:19 +0200

    Comments in CODE/count_set_bits.py

--- CODE/count_set_bits.py
+++ CODE/count_set_bits.py
@@ -1,4 +1,4 @@
-# count_set_bits.py - count set buts in a given integer.
+# count_set_bits.py - count set bits in a given integer.
 
 # LOAD:
 # import sys;  import os;  sys.path.insert(0, os.getcwd());  from count_set_bits import *
@@ -6,10 +6,19 @@
 # RELOAD:
 # import importlib; import count_set_bits; importlib.reload(count_set_bits); from count_set_bits import *
 
+# The idea: n&(n-1) clears the rightmost set bit - bacause -1 inverts all bits after last set bit inclusive.
+# See https://www.geeksforgeeks.org/dsa/count-set-bits-in-an-integer/
+
 # 10              = 00001010
 # 9               = 00001001 -1 inverts all bits after last set bit inclusive
 # 8               = 00001000
 # 7               = 00000111
+# 6               = 00000110
+# 5               = 00000101
+# 4               = 00000100
+# 3               = 00000011
+# 2               = 00000010
+# 1               = 00000001
 
 # 10&9            = 00001000 - least significant set bit is removed
 # 10&9-1          = 00000111
