8ceb9f10aa506a63ed1a47f6b29004f58f0a3386
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-03-11 13:20:22 +0200

    First breathing: CODE/LC01/lc0025__reverse_k_groups.py

--- CODE/LC01/lc0025__reverse_k_groups.py
+++ CODE/LC01/lc0025__reverse_k_groups.py
@@ -1,26 +1,108 @@
 # lc0025__reverse_k_groups.py
+# Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+# You may not alter the values in the list's nodes, only nodes themselves may be changed.
+
+# LOAD:
+# import sys;  import os;  sys.path.insert(0, os.getcwd());  from lc0025__reverse_k_groups import *
+
+# RELOAD:
+# import importlib; import lc0025__reverse_k_groups; importlib.reload(lc0025__reverse_k_groups); from lc0025__reverse_k_groups import *
 
 # The idea: reverse each group by standard list reversal.
-# dummy>1>2>3>4>5>6>0
-#       ^
-# - temporarily insert break after k nodes; save nextGroup ptr
-# dummy>1>2>3>0 nextGroup>4>5>6>0
-#       ^
-# - reverse null-terminated group of k nodes; obtain pointer to the group's new last element
-# dummy>3>2>1>0 nextGroup>4>5>6>0
-#           ^
-# - connect the group's new last element to nextGroup; next group to reverse = nextGroup
-# dummy>3>2>1>4>5>6>0
-#             ^
-# dummy-head-node.next = head
-# elemToConnectGroupTo = dummy-head-node
-# nextGroup = head
-# while ...:
-#   oldGroupHead, oldGroupTail = find_k_group(nextGroup)
-#   # (note that oldGroupHead == nextGroup)
-#   if (oldGroupHead == oldGroupTail):  break  # no more complete groups
-#   nextGroup = oldGroupTail.next
-#   oldGroupTail.next = None  # tmp
-#   reverse_list(oldGroupHead)
-#   elemToConnectGroupTo.next = oldGroupTail
-#   elemToConnectGroupTo = oldGroupHead
+# - Build arrays of group-first- and group-last elements:
+#       +     +     +    - groupFirst
+# dummy>1>2>3>4>5>6>7>8>0, k=3
+#     ^     ^     ^   0  - groupLast
+# (reverse each group individually)
+# - Loop starting from the last _complete_ group:
+#        (the group is complete when groupLast[i] is not None) 
+# -- groupLast[i].next = None  # null-0 terminate to enable reversal
+# -- reverse group #i
+# -- swap groupFirst[i] and groupLast[i]
+# (reconnect the groups)
+# - Loop for each i:
+# -- groupLast[i].next = groupFirst[i+1]
+# - if needed, treat edge cases
+
+from UTILS.lib__linked_list import *
+
+
+def reverse_linked_list(head: Node) -> Node:
+    if ( (head is None) or (head.next is None) ):
+        return(head)
+    prev = None
+    curr = head
+    while ( curr is not None ):
+        next = curr.next
+        #print(f"curr={curr.data}, next={next.data if (next is not None) else 'None'}")
+        curr.next = prev
+        #print(f"Set curr.next to: {curr.next.data if (curr.next is not None) else 'None'}")
+        prev = curr
+        curr = next
+    return(prev)
+
+
+# Returns tuple of arrays groupFirst, groupLast
+# Assumes list is not empty
+def find_groups(listHead: Node, k:int) -> tuple[list[Node], list[Node]]:
+#  0 1 2 3 4 5 6 7
+#  +     +     +    - groupFirst
+# >1>2>3>4>5>6>7>8>0, k=3
+#      ^     ^   0  - groupLast
+    groupFirst = [];  groupLast = []
+    i = 0
+    p = listHead
+    while ( p is not None ):
+        if ( i % k == 0 ):
+            # group starts at current element
+            groupFirst.append(p)
+        if ( (i > 0) and ((i+1) % k == 0) ):
+            # prev group ends at current element
+            groupLast.append(p)
+        elif ( p.next is None ):# last !incomplete! group ends at current element
+            groupLast.append(None)
+        p = p.next
+        i += 1
+    return (groupFirst, groupLast)
+
+
+# Marks group ends with None-s, reverses each group, swaps first-last
+def reverse_in_groups(groupFirst: list[Node], groupLast: list[Node]) -> tuple[list[Node], list[Node]]:
+    if ( len(groupFirst) != len(groupLast) ):
+        raise Exception("Different lengths of group first/last pointers")
+    for i in range(0, len(groupFirst)):
+        if ( groupLast[i] is None ):
+            continue  # skip incomplete (the last) group
+        groupLast[i].next = None  # terminate the group for reversal
+        newHead = reverse_linked_list(groupFirst[i])
+        if ( newHead != groupLast[i] ):
+            raise Exception("newHead != groupLast[i]")
+        # swap groupFirst[i] and groupLast[i]
+        tmp = groupFirst[i]
+        groupFirst[i] = groupLast[i]
+        groupLast[i] = tmp
+    return (groupFirst, groupLast)
+
+
+# Connects end of each group to start of next group.
+# Returns pointer to the head.
+def reconnect_groups(groupFirst: list[Node], groupLast: list[Node]) -> Node:
+    for i in range(0, len(groupLast)-1):
+        if ( groupLast[i] is not None  ): # any group but the last-incomplete
+            groupLast[i].next = groupFirst[i+1]
+    return groupFirst[0]
+
+
+# l1 = LinkedList.from_python_list([1,2,3,4,5,6,7,8])
+#
+# groupFirst, groupLast = find_groups(l1.head, 3)
+# for p in groupFirst:  print(p.data, ">")
+# for p in groupLast:  print(p.data if p is not None else "None", ">")
+#
+# groupFirstR, groupLastR = reverse_in_groups(groupFirst, groupLast)
+# for p in groupFirstR:  print(p.data if p is not None else "None", ">")
+# for p in groupLastR:  print(p.data if p is not None else "None", ">")
+#
+# newHead = reconnect_groups(groupFirstR, groupLastR)
+# print(LinkedList.list_to_string(newHead))
