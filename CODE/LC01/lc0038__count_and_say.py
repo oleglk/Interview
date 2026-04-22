5b5813dd1513de5bc3171b693179795c05778f08
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-04-22 11:57:39 +0300

    Comment in CODE/LC01/lc0038__count_and_say.py

--- CODE/LC01/lc0038__count_and_say.py
+++ CODE/LC01/lc0038__count_and_say.py
@@ -4,6 +4,10 @@
 #   countAndSay(n) is the run-length encoding of countAndSay(n - 1).
 # Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 1 or more times) with the concatenation of the character and the number marking the count of the characters (length of the run); count comes first.
 # For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". Thus the compressed string becomes "23321511".
+# countAndSay(1) = "1"
+# countAndSay(2) = RLE of "1" = "11"
+# countAndSay(3) = RLE of "11" = "21"
+# countAndSay(4) = RLE of "21" = "1211"
 
 # LOAD:
 # import sys;  import os;  sys.path.insert(0, os.getcwd());  from lc0038__count_and_say import *
