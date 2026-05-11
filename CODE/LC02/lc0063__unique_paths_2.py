51fd745cec8d82719730f93bd5c3d84a9d87f830
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-05-11 10:40:22 +0300

    renamed:    CODE/LC02/lc0064__unique_paths_2.py -> CODE/LC02/lc0063__unique_paths_2.py

--- CODE/LC02/lc0063__unique_paths_2.py
+++ CODE/LC02/lc0063__unique_paths_2.py
@@ -1,13 +1,13 @@
-# lc0064__unique_paths_2.py
+# lc0063__unique_paths_2.py
 # You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 # An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
 # Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
 
 # LOAD:
-# import sys;  import os;  sys.path.insert(0, os.getcwd());  from lc0064__unique_paths_2 import *
+# import sys;  import os;  sys.path.insert(0, os.getcwd());  from lc0063__unique_paths_2 import *
 
 # RELOAD:
-# import importlib; import lc0064__unique_paths_2; importlib.reload(lc0064__unique_paths_2); from lc0064__unique_paths_2 import *
+# import importlib; import lc0063__unique_paths_2; importlib.reload(lc0063__unique_paths_2); from lc0063__unique_paths_2 import *
 
 # The idea: use DP. dp[0][i] = dp[i][0] = 1. For other cells dp[i][j] = dp[i-1][j] + dp[i][j-1]. But if #i,j contains obstacle, dp[i][j] == 0. The answer will be in the bottom-right corner.
 
