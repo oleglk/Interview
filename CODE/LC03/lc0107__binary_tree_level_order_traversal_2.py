f4a89431438899b5698468f2a3654133663d373b
Oleg Kosyakovsky <oleg.kosyakovsky@gmail.com>
2026-07-17 11:21:39 +0300

    Working CODE/LC03/lc0107__binary_tree_level_order_traversal_2.py

---
+++ CODE/LC03/lc0107__binary_tree_level_order_traversal_2.py
@@ -0,0 +1,93 @@
+# lc0107__binary_tree_level_order_traversal_2.py
+# Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
+
+# LOAD:
+# import sys;  import os;  sys.path.insert(0, os.getcwd());  from lc0107__binary_tree_level_order_traversal_2 import *
+
+# RELOAD:
+# import importlib;    import lc0107__binary_tree_level_order_traversal_2;  importlib.reload(lc0107__binary_tree_level_order_traversal_2);  from lc0107__binary_tree_level_order_traversal_2 import *
+
+# The idea: fill the queue one level at a time; rely on catching number of elements in the queue when it holds exactly next level. Reverse the resulting list once - before return.
+# See https://algo.monster/liteproblems/107
+
+from collections import deque
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None  # Reference to the left child node
+        self.right = None # Reference to the right child node
+##
+
+
+def binary_tree_level_order_traversal_2(root: Node|None) -> list[int]:
+    if ( root is None ):
+        return []
+    queue = deque([root])
+    result = []
+
+    while ( len(queue) > 0 ):
+        # at this moment queue contains one level
+        cntNodesAtLevel = len(queue)
+        nodesAtLevel = []
+        for _ in range(0, cntNodesAtLevel):
+            node = queue.popleft()
+            nodesAtLevel.append(node.data)
+            # push nodes of the next level
+            if ( node.left is not None ):
+                queue.append(node.left)
+            if ( node.right is not None ):
+                queue.append(node.right)
+        result.append(nodesAtLevel)  # current level s finished
+    return result[::-1]
+##
+
+
+def test__binary_tree_level_order_traversal_2():
+    ##      0
+    ##     / \
+    ##    1   2
+    ##   / \ /
+    ##  3  4 5
+    ## By levels: [[3, 4, 5], [1, 2], [0]]
+    t1n0 = Node(0)
+    t1n1 = Node(1);    t1n2 = Node(2)
+    t1n0.left = t1n1;  t1n0.right = t1n2
+    t1n3 = Node(3);    t1n4 = Node(4);     t1n5 = Node(5)
+    t1n1.left = t1n3;  t1n1.right = t1n4;  t1n2.left = t1n5
+    ##     0
+    ##      \
+    ##       1
+    ##        \
+    ##         2
+    ## By levels: [[2], [1], [0]]
+    t2n0 = Node(0)
+    t2n1 = Node(1);    t2n2 = Node(2)
+    t2n0.right = t2n1
+    t2n1.right = t2n2
+    ##     0
+    ## By levels: [[0]]
+    t3n0 = Node(0)
+    ##      0
+    ##     /
+    ##    1
+    ## By levels: [[1], [0]]
+    t4n0 = Node(0)
+    t4n1 = Node(1)
+    t4n0.left = t4n1
+    ##     1
+    ##    / \
+    ##   0   2
+    ##        \
+    ##         3
+    ## By levels: [[3], [0, 2], [1]]
+    t5n0 = Node(1)
+    t5n1 = Node(0);    t5n2 = Node(2);    t5n3 = Node(3)
+    t5n0.left = t5n1;  t5n0.right = t5n2
+    t5n2.right = t5n3
+
+    tasks = [t1n0, t2n0, t3n0, t4n0, t5n0]
+    for root in tasks:
+        byLevels = binary_tree_level_order_traversal_2(root)
+        print(f"By levels: {byLevels}")
+##
